b419a61e1e00aac6d01534373d2856a9931b42e0
Shahrooz <shahrooz.bazrafshan@gmail.com>
2020-09-15 16:25:57 +0430

    start page

--- GolPooch.Pwa/src/layouts/emptyLayout.jsx
+++ GolPooch.Pwa/src/layouts/emptyLayout.jsx
@@ -1,12 +1,14 @@
 ﻿import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Authorization from '../pages/auth';
+import Start from '../pages/start';
 
 const EmptyLayout = () => {
     return (
-            <Switch>
-                <Route exact path="/" component={Authorization} />
-            </Switch>
+        <Switch>
+            <Route exact path='/' component={Start} />
+            <Route exact path="/auth" component={Authorization} />
+        </Switch>
     );
 }
 export default EmptyLayout;
