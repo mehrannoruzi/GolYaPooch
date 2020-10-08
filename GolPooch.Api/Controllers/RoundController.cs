133be55a81bfb0c2c06eba1f3bc7b6014f8b939d
Mehran Norouzi <mehrannoruzi@gmail.com>
2020-10-09 01:20:50 +0330

    Add Round/Top & Round/MyChance Api

---
+++ GolPooch.Api/Controllers/RoundController.cs
@@ -0,0 +1,27 @@
+﻿using Elk.Core;
+using GolPooch.Domain.Entity;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GolPooch.Service.Interfaces;
+
+namespace GolPooch.Api.Controllers
+{
+    [AuthorizeFilter, Route("[controller]/[action]")]
+    public class RoundController : Controller
+    {
+        private IRoundService _roundService { get; set; }
+
+        public RoundController(IRoundService roundService)
+            => _roundService = roundService;
+
+
+        [HttpGet]
+        public async Task<JsonResult> Top(User user, [FromQuery] PagingParameter pagingParameter)
+            => Json(await _roundService.GetTopRoundsAsync(user.UserId, pagingParameter));
+
+        [HttpGet]
+        public async Task<JsonResult> MyChance([FromQuery] int roundId)
+            => Json(await _roundService.GetChanceInRoundAsync(roundId));
+
+    }
+}
---
+++ GolPooch.Service/Interfaces/Draw/IRoundService.cs
@@ -0,0 +1,12 @@
+﻿using Elk.Core;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace GolPooch.Service.Interfaces
+{
+    public interface IRoundService
+    {
+        Task<IResponse<PagingListDetails<object>>> GetTopRoundsAsync(int userId, PagingParameter pagingParameter);
+        Task<IResponse<List<object>>> GetChanceInRoundAsync(int roundId);
+    }
+}
