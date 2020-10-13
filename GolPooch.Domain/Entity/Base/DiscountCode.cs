47c6ab8b4ddef4d7624635b78f1294c758f39017
Mehran <mehrannoruzi@gmail.com>
2020-10-13 12:58:44 +0330

    Fixed Payment & BeforeChance

--- GolPooch.Domain/Entity/Base/DiscountCode.cs
+++ GolPooch.Domain/Entity/Base/DiscountCode.cs
@@ -29,12 +29,12 @@ namespace GolPooch.Domain.Entity
         public bool IsUsed { get; set; } = false;
 
         [Display(Name = nameof(DisplayNames.InsertDate), ResourceType = typeof(DisplayNames))]
-        public DateTime UsedDateMi { get; set; }
+        public DateTime? UsedDateMi { get; set; }
 
         [Column(TypeName = "char(10)")]
         [Display(Name = nameof(DisplayNames.InsertDate), ResourceType = typeof(DisplayNames))]
         [MaxLength(10, ErrorMessageResourceName = nameof(ErrorMessage.MaxLength), ErrorMessageResourceType = typeof(ErrorMessage))]
-        public string UsedDateSh { get; set; }
+        public string? UsedDateSh { get; set; }
 
         [Column(TypeName = "varchar(16)")]
         [Display(Name = nameof(DisplayNames.Code), ResourceType = typeof(DisplayNames))]
