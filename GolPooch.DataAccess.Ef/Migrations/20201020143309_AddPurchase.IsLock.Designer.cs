9527de1d6dbbe11ccdee34f4741624b99130e46c
Mehran <mehrannoruzi@gmail.com>
2020-10-20 18:34:20 +0330

    Add Purchase.IsLock Property

--- GolPooch.DataAccess.Ef/Migrations/20201020143309_AddPurchase.IsLock.Designer.cs
+++ GolPooch.DataAccess.Ef/Migrations/20201020143309_AddPurchase.IsLock.Designer.cs
@@ -4,14 +4,16 @@ using GolPooch.DataAccess.Ef;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GolPooch.DataAccess.Ef.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201020143309_AddPurchase.IsLock")]
+    partial class AddPurchaseIsLock
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
